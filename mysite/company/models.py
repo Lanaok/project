d1529ffb86f69b27a0971c165e88b84924302a91
Lana Okropiridze <lokro15@freeuni.edu.ge>
2020-07-19 14:40:44 +0400

    adding products and company_type

--- mysite/company/models.py
+++ mysite/company/models.py
@@ -6,11 +6,19 @@ from django.urls import reverse
 from django.utils import timezone
 
 
+
+
 class Company(models.Model):
+    Company_Type = (
+        ('R', 'Restaurant/cafe'),
+        ('M', 'Hospital/Dentistry'),
+        ('S', 'Beauty Salon'),
+    )
     name = models.CharField(max_length=50)
     manager = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     description = models.TextField(max_length=500)
     date_created = models.DateField(default=timezone.now)
+    company_type = models.CharField(max_length=1, choices=Company_Type, default='R')
 
     class Meta:
         permissions = (("can_edit_company", "Edit company info"),)
@@ -21,6 +29,11 @@ class Company(models.Model):
     def get_absolute_url(self):
         return reverse('company-detail', args=[str(self.id)])
 
+class Products(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_description = models.TextField(max_length=500)
+    photos = models.ImageField()
+    company_products = models.ForeignKey(Company, null=True, on_delete=models.SET_NULL)
 
 class StaffMember(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)

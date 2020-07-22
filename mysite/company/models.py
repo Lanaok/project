7c6df5d33ecfcf5d51da43f1edb9198f2fbd90d1
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2020-07-22 10:57:15 +0400

    modified models

--- mysite/company/models.py
+++ mysite/company/models.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
 from django.urls import reverse
 from django.utils import timezone
 
-
+from profile.models import Profile
 
 
 class Company(models.Model):
@@ -15,10 +14,11 @@ class Company(models.Model):
         ('S', 'Beauty Salon'),
     )
     name = models.CharField(max_length=50)
+    manager = models.OneToOneField(Profile, null=True, on_delete=models.SET_NULL)
     description = models.TextField(max_length=500)
-    date_created = models.DateField(default=timezone.now)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
     company_type = models.CharField(max_length=1, choices=Company_Type, default='R')
-
 
     class Meta:
         permissions = (("can_edit_company", "Edit company info"),)
@@ -29,15 +29,25 @@ class Company(models.Model):
     def get_absolute_url(self):
         return reverse('company-detail', args=[str(self.id)])
 
+
 class Products(models.Model):
     product_name = models.CharField(max_length=50)
     product_description = models.TextField(max_length=500)
     photos = models.ImageField()
-    company_products = models.ForeignKey(Company, null=True, on_delete=models.SET_NULL)
+    company = models.ForeignKey(Company, null=True, on_delete=models.SET_NULL)
 
 
+class Service(models.Model):
+    service_name = models.CharField(max_length=50)
+    service_description = models.TextField(max_length=200)
+    service_price = models.IntegerField()
+    service_duration = models.DurationField()
+    service_image = models.ImageField()
+    company = models.ManyToManyField(Company)
 
-# class StaffMember(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-#     date_created = models.DateField(default=timezone.now)
+
+class StaffMember(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    services = models.ManyToManyField(Service)
+    date_joined = models.DateTimeField(default=timezone.now)
--- mysite/profile/models.py
+++ mysite/profile/models.py
@@ -4,24 +4,19 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
-from company.models import Company
-
-class Service(models.Model):
-    service_name = models.CharField(max_length=50)
-    service_description = models.TextField(max_length=200)
-    service_price = models.IntegerField()
-    service_duration = models.DurationField()
-    service_image = models.ImageField()
-    company_services = models.ManyToManyField(Company, through='Profile')
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(null=True, blank=True)
-    phone_number = models.CharField(max_length=20)
-    staff_at = models.ForeignKey(Company, null=True, on_delete=models.SET_NULL, blank=True, related_name="staff")
-    staff_services = models.ForeignKey(Service, null=True, on_delete=models.SET_NULL, blank=True)
-    date_created = models.DateField(default=timezone.now)
-    date_updated = models.DateField(default=timezone.now)
+    phone_number = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
+
+
+class Manager(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
 
 
 @receiver(post_save, sender=User)
@@ -34,4 +29,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.date_updated = timezone.now()
     instance.profile.save()
-

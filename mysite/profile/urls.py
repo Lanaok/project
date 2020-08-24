28fc662569e8b4e8837828dede61dd789e1eda4e
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2020-08-24 20:46:47 +0400

    added denied filter, fixed order details link

--- mysite/profile/urls.py
+++ mysite/profile/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     # path('', views.contact),
     path('update/', views.update_profile, name='profile-update'),
     path('', views.view_profile, name='profile-detail'),
-    path('orders/<str:filter>/', views.ProfileOrderList.as_view(), name='profile-orders'),
     path('orders/<int:order_id>/', order_view, name='order-view'),
+    path('orders/<str:filter>/', views.ProfileOrderList.as_view(), name='profile-orders'),
     path('<int:order_id>/exchange/', order_change, name='order_change'),
     path('<int:order_id>/remove/', order_remove, name='order_remove'),
 ]

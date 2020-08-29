301c4fbbc247d1548bbfdea4dd285fd43789081d
irakli mghebrishvili <imghe15@freeuni.edu.ge>
2020-08-29 15:16:46 +0400

    fixed loading order page. order day comes first, time comes after that in order form

--- mysite/order/forms.py
+++ mysite/order/forms.py
@@ -7,9 +7,8 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        exclude = ['user_orders',  'staff_order', 'date_created', 'service_order', 'order_state', 'order_message']
-        fields = ['order_time', 'order_day']
+        fields = ['order_day', 'order_time']
         widgets = {
-            'order_time': DateInput(attrs={'class': 'form-control', 'type': 'time'}),
             'order_day': DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'order_time': DateInput(attrs={'class': 'form-control', 'type': 'time'}),
         }
